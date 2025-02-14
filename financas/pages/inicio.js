8b8643809cf52b37cdaa0116571d1aaeb27e7231
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2025-02-14 20:31:07 -0300

    Callback para atualizar lista automaticamente

--- financas/pages/inicio.js
+++ financas/pages/inicio.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button} from 'react-native';
+import { View, TextInput, Button } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Inicio = () => {
   const [texto, setTexto] = useState('');
+  const [valor, setValor] = useState('');
+  const [data, setData] = useState('');
 
-  const armazenarTexto = async () => {
+  const armazenarDados = async () => {
     try {
-      const chave = `texto-${Date.now()}`;
-      await AsyncStorage.setItem(chave, texto);
+      const chave = `dados-${Date.now()}`;
+      const dados = { texto, valor, data };
+      await AsyncStorage.setItem(chave, JSON.stringify(dados));
       setTexto('');
+      setValor('');
+      setData('');
     } catch (error) {
       alert(error);
     }
@@ -20,9 +25,20 @@ const Inicio = () => {
       <TextInput
         value={texto}
         onChangeText={(text) => setTexto(text)}
-        placeholder="Digite um texto"
+        placeholder="Digite a manutenção:"
       />
-      <Button title="Armazenar" onPress={armazenarTexto} />
+      <TextInput
+        value={valor}
+        onChangeText={(text) => setValor(text)}
+        placeholder="Digite um valor:"
+        keyboardType="numeric"
+      />
+      <TextInput
+        value={data}
+        onChangeText={(text) => setData(text)}
+        placeholder="Digite uma data:"
+      />
+      <Button title="Armazenar" onPress={armazenarDados} />
     </View>
   );
 };
--- financas/pages/lista.js
+++ financas/pages/lista.js
@@ -1,42 +1,52 @@
-    import React, { useState } from 'react';
-    import { View, Text, Button, FlatList } from 'react-native';
-    import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, FlatList } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 
-    const Lista = () => {
-    const [textosArmazenados, setTextosArmazenados] = useState([]);
-    const recuperarTextos = async () => {
+const Lista = () => {
+  const [itensArmazenados, setItensArmazenados] = useState([]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const recuperarItens = async () => {
         try {
-        const chaves = await AsyncStorage.getAllKeys();
-        const textos = await AsyncStorage.multiGet(chaves);
-        const textosArmazenados = textos.map((texto) => texto[1]);
-        setTextosArmazenados(textosArmazenados);
+          const chaves = await AsyncStorage.getAllKeys();
+          const itens = await AsyncStorage.multiGet(chaves);
+          const itensArmazenados = itens.map((item) => JSON.parse(item[1]));
+          setItensArmazenados(itensArmazenados);
         } catch (error) {
-        alert(error);
+          alert(error);
         }
-    };
+      };
+      recuperarItens();
+    }, [])
+  );
 
-    const excluirTodosItens = async () => {
-        try {
-        await AsyncStorage.clear();
-        setTextosArmazenados([]);
-        } catch (error) {
-        alert(error);
-        }
-    };
+  const excluirTodosItens = async () => {
+    try {
+      await AsyncStorage.clear();
+      setItensArmazenados([]);
+    } catch (error) {
+      alert(error);
+    }
+  };
 
-    return (
-        <View style={{ padding: 50 }}>
-        <Button title="Recuperar" onPress={recuperarTextos} />
-        <FlatList
-            data={textosArmazenados}
-            renderItem={({ item }) => (
-            <Text style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: '#ccc' }}>{item}</Text>
-            )}
-            keyExtractor={(item, index) => index.toString()}
-        />
-        <Button title="Excluir todos os itens" onPress={excluirTodosItens} />
-        </View>
-    );
-    };
+  return (
+    <View style={{ padding: 50 }}>
+      <FlatList
+        data={itensArmazenados}
+        renderItem={({ item }) => (
+          <View>
+            <Text>Manutenção: {item.texto}</Text>
+            <Text>Valor: R${item.valor}</Text>
+            <Text>Data: {item.data} {'\n'}</Text>
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <Button title="Excluir todos os itens da lista" onPress={excluirTodosItens} />
+    </View>
+  );
+};
 
-    export default Lista;
+export default Lista;
