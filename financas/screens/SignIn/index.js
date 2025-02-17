f7ac2687cb3ba3a48c67d904d4299413536014fa
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2025-02-17 17:58:44 -0300

    Update index.js

--- financas/screens/SignIn/index.js
+++ financas/screens/SignIn/index.js
@@ -3,12 +3,23 @@ import { ImageBackground, View, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Button } from "react-native";
 import styles from "../../styles/styles";
+import * as AuthSession from 'expo-auth-session';
 
 const SignIn = () => {
   const navigation = useNavigation();
 
   async function handleSignIn() {
-    navigation.navigate('Inicio');
+    const CLIENT_ID ='351770148352-ap99k6mvur2b1ncp1sjf6q8jt3tka5ab.apps.googleusercontent.com';
+    const REDIRECT_URI = 'https://auth.expo.io/@felipeppt-2/financas';
+    const RESPONSE_TYPE = 'token';
+    const SCOPE = encodeURI('inicio email');
+
+
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
+    const response = await AuthSession.startAsync({authUrl})
+    console.log(response)
+
+    navigation.navigate('Inicio/index');
   }
 
   return (
